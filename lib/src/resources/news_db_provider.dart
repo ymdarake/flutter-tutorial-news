ef039b1e1cbd18c7cb99e7411a32303d3601676b
ymdarake <t.sakamoto.ouen@gmail.com>
2019-12-27 20:58:19 +1300

    init

---
+++ lib/src/resources/news_db_provider.dart
@@ -0,0 +1,56 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import '../models/item_model.dart';
+
+class NewsDbProvider {
+  Database db;
+
+  void init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, "items.db");
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        newDb.execute("""
+          CREATE TABLE Items
+            (
+              id INTEGER PRIMARY KEY,
+              type TEXT,
+              by TEXT,
+              time INTEGER,
+              text TEXT,
+              parent INTEGER,
+              kids BLOB,
+              dead INTEGER,
+              deleted INTEGER,
+              url TEXT,
+              score INTEGER,
+              title TEXT,
+              descendants INTEGER
+            )
+        """);
+      },
+    );
+  }
+
+  Future<ItemModel> fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null /** all columns */, // ["title"]
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    if (maps.length == 0) return null;
+
+    return ItemModel.fromDb(maps.first);
+  }
+
+  Future<int> addItem(ItemModel item) {
+    return db.insert("Items", item.toMapForDb());
+  }
+}
