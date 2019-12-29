0e147313d8cbe5bfb049d8707033a8c293a9e78c
ymdarake <t.sakamoto.ouen@gmail.com>
2019-12-29 18:21:35 +1300

    [280]Wrapping Up Future Builder

---
+++ lib/src/widgets/news_list_tile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/stories_provider.dart';
+
+class NewsListTile extends StatelessWidget {
+  final int itemId;
+
+  NewsListTile({this.itemId});
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+    return StreamBuilder(
+      stream: bloc.items,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Stream still loading');
+        }
+
+        return FutureBuilder(
+          future: snapshot.data[itemId],
+          builder: (context, AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              return Text('Item still loading $itemId');
+            }
+
+            return Text(itemSnapshot.data.title);
+          },
+        );
+      },
+    );
+  }
+}
