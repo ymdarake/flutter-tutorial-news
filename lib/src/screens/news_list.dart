78e4271fe4c6f53093290253ddbf38d2d6c4989e
ymdarake <t.sakamoto.ouen@gmail.com>
2019-12-29 16:43:12 +1300

    [add]266: Bloc Testing

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,40 +1,35 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show a list'),
+      body: buildList(bloc),
     );
   }
 
-  Widget buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, int index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot) {
-            return Container(
-              height: 80.0,
-              child: snapshot.hasData
-                  ? Text('Im visible $index')
-                  : Text('I havent fetched data $index'),
-            );
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Still waiting on Ids');
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            return Text(snapshot.data[index]);
           },
         );
       },
-    );
-  }
-
-  Future getFuture() {
-    return Future.delayed(
-      Duration(seconds: 2),
-      () => 'hi',
     );
   }
 }
