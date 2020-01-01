090b0b19b6da5e11acc993f8638597888e736716
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-01 18:40:55 +1300

    [add]Displaying the Story Tile (data fetching)

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../blocs/comments_provider.dart';
+import '../models/item_model.dart';
 
 class NewsDetail extends StatelessWidget {
   final int itemId;
@@ -7,11 +9,36 @@ class NewsDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bloc = CommentsProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('detail'),
       ),
-      body: Text('$itemId'),
+      body: buildBody(bloc),
+    );
+  }
+
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemWithComments,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('loading');
+        }
+        final itemFuture = snapshot.data[itemId];
+
+        return FutureBuilder(
+          future: itemFuture,
+          builder: (context, AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              return Text('Loading');
+            }
+
+            return Text(itemSnapshot.data.title);
+          },
+        );
+      },
     );
   }
 }
