8ab8614a9327b99d68de8a8b0ab83ffe8fe5952b
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-01 19:23:42 +1300

    [add]327: Nested comment

--- lib/src/widgets/comment.dart
+++ lib/src/widgets/comment.dart
@@ -4,8 +4,9 @@ import '../models/item_model.dart';
 class Comment extends StatelessWidget {
   final int itemId;
   final Map<int, Future<ItemModel>> itemMap;
+  final int depth;
 
-  Comment({this.itemId, this.itemMap});
+  Comment({this.itemId, this.itemMap, this.depth});
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +27,7 @@ class Comment extends StatelessWidget {
         ];
         snapshot.data.kids.forEach((kidId) {
           children.add(
-            Comment(itemId: kidId, itemMap: itemMap),
+            Comment(itemId: kidId, itemMap: itemMap, depth: depth + 1),
           );
         });
 
