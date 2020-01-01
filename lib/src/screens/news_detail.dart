a9b32e4ed5d4734a78b3c82181ab8bc77439be77
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-01 19:08:22 +1300

    [add]322: Showing individual comments

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../blocs/comments_provider.dart';
 import '../models/item_model.dart';
+import '../widgets/comment.dart';
 
 class NewsDetail extends StatelessWidget {
   final int itemId;
@@ -35,10 +36,23 @@ class NewsDetail extends StatelessWidget {
               return Text('Loading');
             }
 
-            return buildTitle(itemSnapshot.data);
+            return buildList(itemSnapshot.data, snapshot.data);
           },
         );
       },
+    );
+  }
+
+  Widget buildList(ItemModel item, Map<int, Future<ItemModel>> itemMap) {
+    final children = <Widget>[];
+    children.add(buildTitle(item));
+    final commentsList = item.kids.map((kidId) {
+      return Comment(itemId: kidId, itemMap: itemMap);
+    }).toList();
+    children.addAll(commentsList);
+
+    return ListView(
+      children: children,
     );
   }
 
